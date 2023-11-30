0ffe9adeff4fae821855fed2f636c3cb91e69f00
Evaldo-Gomes <evaldo_gomes@hotmail.com>
2023-11-30 14:37:51 -0300

    Primeiro commit

--- calc2.py
+++ calc2.py
@@ -6,6 +6,10 @@ def soma(n1, n2):
 def sub(n1, n2):
     return n1 - n2
 
+def div(n1, n2):
+    return n1/n2
+
 #teste
 #print(f'{2} + {2} = {soma(2, 2)}')
 #print(f'{2} - {2} = {sub(2, 2)}')
+print(f'{10} / {2} = {div(10, 2)}')
