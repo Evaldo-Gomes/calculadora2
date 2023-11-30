c44c5e38179f3de7c16737ff2eea002e875ca23e
CrisYuri <cristina.yuri@gmail.com>
2023-11-30 14:43:12 -0300

    - Incluído totais (Cris)

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 # Interface da Calculadora
 
-from calc2 import soma
+import calc2
 
 #Apresentação
 print('\n\t\t\t  --  Calculadora 2  --\n')
@@ -10,7 +10,11 @@ num1 = int(input('Informe o n1: '))
 num2 = int(input('Informe o n2: '))
 
 # Processamento
-total = soma(num1, num2)
+totalSoma = soma(num1, num2)
+totalSub = sub(num1, num2)
+totalMult = mult(num1, num2)
 
 # Saída
-print(f'Resultado: {num1} + {num2} = {total}')
+print(f'Resultado da soma: {num1} + {num2} = {totalSoma}')
+print(f'Resultado da subtração: {num1} - {num2} = {totalSub}')
+print(f'Resultado da multiplicação: {num1} * {num2} = {totalMult}')
