e5387048826d87e95330630fea0685e765184e1e
Evaldo-Gomes <evaldo_gomes@hotmail.com>
2023-11-30 14:38:59 -0300

    Incluído função da divisão

--- calc2.py
+++ calc2.py
@@ -10,6 +10,6 @@ def div(n1, n2):
     return n1/n2
 
 #teste
-#print(f'{2} + {2} = {soma(2, 2)}')
-#print(f'{2} - {2} = {sub(2, 2)}')
+print(f'{2} + {2} = {soma(2, 2)}')
+print(f'{2} - {2} = {sub(2, 2)}')
 print(f'{10} / {2} = {div(10, 2)}')
