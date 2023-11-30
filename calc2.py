3f368c7ff7584434817d694d80164995ef60f392
CrisYuri <cristina.yuri@gmail.com>
2023-11-30 14:32:31 -0300

    - Incluído def multiplicação (Cris)

--- calc2.py
+++ calc2.py
@@ -6,6 +6,9 @@ def soma(n1, n2):
 def sub(n1, n2):
     return n1 - n2
 
+def mult(n1, n2):
+    return n1 * n2
 #teste
 #print(f'{2} + {2} = {soma(2, 2)}')
 #print(f'{2} - {2} = {sub(2, 2)}')
+print(f'{2} * {2} = {mult(2, 2)}')
